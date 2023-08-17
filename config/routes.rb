a980cdf13e7e19f9c3ee7568957ec49a2c974e47
Muhammad Amir <m.amir.dev7@gmail.com>
2023-08-17 12:58:30 +0500

    add user session and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  resources :sessions, only: %i[new create]
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
+  resources :users, only: %i[new create]
+
+  root 'recipes#index'
 end
