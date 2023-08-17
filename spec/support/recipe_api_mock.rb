c5b676a05b0a4cc586e7e1abf4013bc664dd8536
Muhammad Amir <m.amir.dev7@gmail.com>
2023-08-17 22:07:30 +0500

    implement recipe spec

--- spec/support/recipe_api_mock.rb
+++ spec/support/recipe_api_mock.rb
@@ -17,7 +17,6 @@ RSpec.configure do |config|
 
     stub_request(:get, "#{base_uri}/complexSearch?apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&number=20&query=")
       .to_return(status: 200, body: response, headers: { 'Content-Type': 'application/json' })
-    # Stub GET request to Spoonacular API
     stub_request(:get, "#{base_uri}/1/information?apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}")
       .to_return(status: 200, body: { "id": 1, "title": 'Cannellini Bean and Asparagus Salad with Mushrooms',
                                       "image": 'spec/fixtures/rice.jpg', "imageType": 'jpg',
@@ -28,35 +27,31 @@ RSpec.configure do |config|
                                       ],
                                       'instructions' => 'Instructions for the recipe.' }.to_json, headers: { 'Content-Type': 'application/json' })
 
-    # Stub GET request to Spoonacular API
     stub_request(:get, "#{base_uri}/complexSearch?apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&diet=&number=20&query=")
       .to_return(status: 200, body: response, headers: { 'Content-Type': 'application/json' })
 
-    # Stub GET request to Spoonacular API
     stub_request(:get, "#{base_uri}/complexSearch?diet='vegetarian'&apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&number=20")
       .to_return(status: 200, body: response, headers: { 'Content-Type': 'application/json' })
 
-    # Stub GET request to Spoonacular API
     stub_request(:get, "#{base_uri}/complexSearch?diet='vegan'&apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&number=20")
       .to_return(status: 200, body: response, headers: { 'Content-Type': 'application/json' })
 
-    # Stub GET request to Spoonacular API
     stub_request(:get, "#{base_uri}/complexSearch?diet='gluten free,vegan'&apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&number=20")
       .to_return(status: 200, body: response, headers: { 'Content-Type': 'application/json' })
 
-    # Stub GET request to Spoonacular API
     stub_request(:get, "#{base_uri}/complexSearch?diet='vegetarian,vegan'&apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&number=20")
       .to_return(status: 200, body: response, headers: { 'Content-Type': 'application/json' })
 
-    # Stub GET request to Spoonacular API
     stub_request(:get, "#{base_uri}/complexSearch?diet='gluten free,vegetarian'&apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&number=20")
       .to_return(status: 200, body: response, headers: { 'Content-Type': 'application/json' })
 
-    # Stub GET request to Spoonacular API
     stub_request(:get, "#{base_uri}/complexSearch?diet='gluten free,vegetarian,vegan'&apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&number=20")
       .to_return(status: 200, body: response, headers: { 'Content-Type': 'application/json' })
 
     stub_request(:get, "#{base_uri}/complexSearch?query=rice&apiKey=#{ENV.fetch('SPOONACULAR_API_KEY')}&number=20")
-      .to_return(status: 200, body: '{"results":[{"id":1,"title":"Rice Cannellini Bean and Asparagus Salad with Mushrooms","image":"spec/fixtures/rice.jpg","imageType":"jpg"},{"id":2,"title":"Rice Cauliflower, Brown Rice, and Vegetable Fried Rice","image":"spec/fixtures/rice.jpg","imageType":"jpg"}]}')
+      .to_return(status: 200, body: { "results": [
+        { "id": 1, "title": 'Rice Cannellini Bean and Asparagus Salad with Mushrooms', "image": 'spec/fixtures/rice.jpg',
+          "imageType": 'jpg' }, { "id": 2, "title": 'Rice Cauliflower, Brown Rice, and Vegetable Fried Rice', "image": 'spec/fixtures/rice.jpg', "imageType": 'jpg' }
+      ] }.to_json, headers: { 'Content-Type': 'application/json' })
   end
 end
