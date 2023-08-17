59efe061e38acc9434dc12fcda36cbe79d7d9b9e
Muhammad Amir <m.amir.dev7@gmail.com>
2023-08-17 21:33:44 +0500

    implement user registration spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,40 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  describe 'before_action :require_login' do
-    context 'when user is logged in' do
-      let(:user) { create(:user) }
-
-      before do
-        session[:user_id] = user.id
-        get :index
-      end
-
-      it 'does not set flash message' do
-        expect(flash[:notice]).to be_nil
-      end
-
-      it 'sets @current_user' do
-        expect(assigns(:current_user)).to eq(user)
-      end
-    end
-
-    context 'when user is not logged in' do
-      before do
-        session[:user_id] = nil
-        get :index
-      end
-
-      it 'sets flash message' do
-        expect(flash[:notice]).to eq('You must be logged in to access this page.')
-      end
-
-      it 'redirects to new session path' do
-        expect(response).to redirect_to(new_session_path)
-      end
-    end
-  end
-
   describe '#current_user' do
     context 'when user is logged in' do
       let(:user) { create(:user) }
